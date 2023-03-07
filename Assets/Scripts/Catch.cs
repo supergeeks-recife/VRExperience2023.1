4bd7cca60f7589b21ad9c70619609dfb9892269e
kleberkgns <kleber.silva.kgns@gmail.com>
2023-03-07 15:32:36 -0300

    contador

---
+++ Assets/Scripts/Catch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catch : MonoBehaviour
+{
+    Counter contadorDePorcos;
+
+    void Start()
+    {
+        contadorDePorcos = FindObjectOfType<Counter>();
+    }
+
+    void OnCollisionEnter(Collision objeto)
+    {
+        if(objeto.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Bateu!");
+            contadorDePorcos.Increment();
+        }
+    }
+}
