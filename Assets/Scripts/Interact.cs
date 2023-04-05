aa5b3fe776b6c020f675c8a016e5b64059ee8f5a
kleberkgns <kleber.silva.kgns@gmail.com>
2023-04-05 15:38:06 -0300

    GVR Raycast Interaction

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Interact : MonoBehaviour
+{
+    public Button startButton;
+    public Image circleProgress;
+    public float totalTime = 3;
+    private bool gvrStatus;
+    private float gvrTimer;
+
+    public UnityEvent gvrClick;
+
+    void Update()
+    {
+        if(gvrStatus == true)
+        {
+            gvrTimer += Time.deltaTime;
+            circleProgress.fillAmount = gvrTimer / totalTime;
+
+            if(gvrTimer > totalTime)
+            {
+                gvrClick.Invoke();
+            }
+        }
+        else
+        {
+            gvrTimer = 0;
+            circleProgress.fillAmount = 0;
+        }
+    }
+
+    public void GvrOnButton()
+    {
+        gvrStatus = true;
+    }
+
+    public void GvrOffButton()
+    {
+        gvrStatus = false;
+    }
+}
